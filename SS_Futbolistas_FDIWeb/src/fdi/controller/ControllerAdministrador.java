d894699a9edf31cb17f04e67119b232c075fa885
CuryT1993 <curytravez_t.v@hotmail.com>
2018-01-21 22:10:05 -0500

    ControllerRol
    Crud Rol

---
+++ SS_Futbolistas_FDIWeb/src/fdi/controller/ControllerAdministrador.java
@@ -0,0 +1,18 @@
+package fdi.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ControllerAdministrador {
+	
+	
+	 private long codRol;    
+	 private String nombrerol;    
+	 private String descripcionrol;    
+	 
+ 
+
+
+}
