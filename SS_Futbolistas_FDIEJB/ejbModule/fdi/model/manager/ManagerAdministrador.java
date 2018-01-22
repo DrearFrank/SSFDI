891ac01952b142f5ee86635226dbcea73a6e615f
CuryT1993 <curytravez_t.v@hotmail.com>
2018-01-21 19:46:15 -0500

    Mapeo de tablas
    creacion de Session beans

---
+++ SS_Futbolistas_FDIEJB/ejbModule/fdi/model/manager/ManagerAdministrador.java
@@ -0,0 +1,20 @@
+package fdi.model.manager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerAdministrador
+ */
+@Stateless
+@LocalBean
+public class ManagerAdministrador {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerAdministrador() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ SS_Futbolistas_FDIEJB/ejbModule/fdi/model/manager/ManagerMedico.java
@@ -0,0 +1,20 @@
+package fdi.model.manager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerMedico
+ */
+@Stateless
+@LocalBean
+public class ManagerMedico {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerMedico() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
